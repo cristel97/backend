e9766e311d8ba715374d8ff7221395c553ec782f
Cristel Tapia <cristel.tch@gmail.com>
2022-10-20 15:01:14 -0500

    ajustes

--- d7/server.js
+++ d7/server.js
@@ -18,6 +18,7 @@ app.get("*", (req, res) => {
     res.json("Error: Ruta o metodo no implementado");
 });
 
+//
 const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`);
