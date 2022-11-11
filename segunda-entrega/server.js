d04504b54b858cd0f7d24b2883d631a1cbd522de
Cristel Tapia <cristel.tch@gmail.com>
2022-11-11 14:08:02 -0500

    error :(

--- segunda-entrega/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { connect } = require("mongoose");
-const routerProducts = require('./src/routes/routerProducts.js');
-const routerCarts = require('./src/routes/routerCarts.js');
-const mongoose = require("mongoose");
-
-const app = express();
-const PORT = process.env.PORT || 8080;
